5cb7cf3104ad6f47a831f914437b366b753b5511
ksaittis <ksaittis@gmail.com>
2020-02-09 17:47:18 +0000

    Added list comprehension example

--- multiprocessing/multiprocessing_experiment.py
+++ multiprocessing/multiprocessing_experiment.py
@@ -1,28 +1,40 @@
+import concurrent.futures
 import time
 import multiprocessing
 
-start = time.perf_counter()
 
-
-def do_something(seconds: int) -> None:
+def do_something(seconds: int) -> str:
     print(f'Sleeping for {seconds}')
     time.sleep(seconds)
-    print(f'Finished sleep')
+    return 'Done sleeping...'
 
 
 def main():
+    start = time.perf_counter()
+
+    use_concurrent_module(10)
+    # use_multiprocessing_module()
+
+    finish = time.perf_counter()
+
+    print(f'Time taken to run is {round(finish - start, 2)}')
+
+
+def use_concurrent_module(num_processes: int) -> None:
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = [executor.submit(do_something, 1) for _ in range(num_processes)]
+        for f in concurrent.futures.as_completed(futures):
+            print(f.result())
+
+
+def use_multiprocessing_module():
     processes = []
     for _ in range(10):
         p = multiprocessing.Process(target=do_something, args=[1])
         p.start()
         processes.append(p)
-
     for p in processes:
         p.join()
-
-    finish = time.perf_counter()
-
-    print(f'Time taken to run is {round(finish - start, 2)}')
 
 
 if __name__ == '__main__':
